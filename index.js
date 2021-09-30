54549686cfcca0898b3893e394f2fa679cb82c5c
TusharSaini24 <sainitushar239@gmail.com>
2021-09-30 17:56:05 +0530

    http server

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("Hello From Node");
-console.log("new console");
+const http = require('http')
+const chalk = require('chalk');
+// const keys = require('./keys')
+
+// console.log(keys);
+
+const server = http.createServer((req,res) => {
+    console.log(chalk.yellow("Server Connected"));
+    // console.log(req)
+    if(req.url == '/') {
+        res.write('<h1>Hello Welcome</h1>')
+        res.end();
+    }
+    else if (req.url == '/about') {
+        res.write('<h1>About Page</h1>')
+        res.end();
+    }
+
+})
+
+server.listen(8000)
